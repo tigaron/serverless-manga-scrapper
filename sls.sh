1f90f2ecdceb317aa5fc2a3495329f4f49cc319d
tigaron <suputra@mail.com>
2022-09-08 00:58:17 +0800

    WIP: Update serverless config

---
+++ sls.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# yarn workspace root run prod &&
+yarn workspace scraper run prod &&
+yarn workspace cron run prod &&
+yarn workspace series run prod &&
+yarn workspace search run prod &&
+yarn workspace status run prod &&
+yarn workspace chapter run prod
